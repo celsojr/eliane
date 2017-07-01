a452ac1a7d58935f0e20267014fb1d98b34d9d6d
Celso <celsojrfull@gmail.com>
2017-07-01 14:38:23 -0300

    Favicon url

--- header.php
+++ header.php
@@ -15,8 +15,8 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="<?php echo get_option( 'siteurl' ); ?>/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="<?php echo get_option( 'siteurl' ); ?>/favicon.ico" type="image/x-icon">
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
